dd95922c8718f1b344f0c1d79e5357e8db20fe1c
kennbeckloff <kennbeckloff688@gmail.com>
2022-10-06 17:17:31 +0300

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     def create
-        user = User.find(username: params[:username])
+        user = User.find_by(username: params[:username])
         session[:user_id] = user.id 
         render json: user
     end
